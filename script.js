4ef82ff6c19b38595f3a7b56cc50acfd33228017
HossainRafi <hossainrafi71@gmail.com>
2023-06-07 15:40:41 +0600

    upload and read file

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ window.onload = () => {
   options.classList.add("hide");
   previewButton.classList.add("hide");
 };
+
+// Upload and read file ===================================================
+fileInput.onchange = () => {
+  //The FileReader object helps tp read contents of file stored on computer
+  let reader = new FileReader();
+  //readAsDataURL read the content of input file
+  reader.readAsDataURL(fileInput.files[0]);
+  reader.onload = () => {
+    //onload is triggered after file reading operation is successfully completed
+    //set src attribute of image to the result/input file
+    image.setAttribute("src", reader.result);
+    //set filename for setting downloaded file name later
+    //initialize cropper
+    if (cropper) {
+      cropper.destroy();
+    }
+    cropper = new Cropper(image);
+    options.classList.remove("hide");
+    previewButton.classList.remove("hide");
+  };
+  fileName = fileInput.files[0].name.split(".")[0];
+};
